effdee2e37e6318ca3f8db834d8e75a33e6f5b1b
Rue Laurito <cal707@nyu.edu>
2019-03-14 10:16:32 -0400

    fixed the continue button

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour {
 
     public Dialogue dialogue;
+    public bool bedTrigger;
 
+    public void Start()
+    {
+        bedTrigger = false;
+
+    }
     public void TriggerDialogue ()
     {
+    
+        //if (bedTrigger == true)
+        
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
