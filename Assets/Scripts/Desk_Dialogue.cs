55aaed61bbee96b52c3de5883324f1c490848533
Rue Laurito <cal707@nyu.edu>
2019-03-15 20:54:50 -0400

    added mirror, desk, fixed raycast scripts;

---
+++ Assets/Scripts/Desk_Dialogue.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desk_Dialogue : MonoBehaviour
+{
+    private bool isTriggered;
+    public GameObject hitObject;
+    public Dialogue deskDialogue;
+    public DialogueManager dialogueManager;
+    public RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>().GetComponent<DialogueManager>();
+        TriggerDialogue();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            hitObject = hit.transform.gameObject;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (hitObject.tag == "Desk")
+                {
+                    TriggerDialogue();
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        ContinueDialogue();
+                    }
+                    print("It works.");
+                }
+            }
+        }
+        
+    }
+    
+    public void TriggerDialogue()
+    {
+        dialogueManager.StartDialogue(deskDialogue);
+    }
+    
+    public void ContinueDialogue()
+    {
+        dialogueManager.DisplayNextSentence();
+    }
+
+}
